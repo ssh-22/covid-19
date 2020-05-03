7de381fa283b5c36c6062e719c599e9f138ae584
ssh-22 <r.march.13_liberty0@yahoo.ne.jp>
2020-05-03 14:52:18 +0900

    Alter counter from string to object

--- frontend/covid-19/src/components/Countdown.tsx
+++ frontend/covid-19/src/components/Countdown.tsx
@@ -7,7 +7,12 @@ type CountdownProps = {
 };
 
 const Countdown = (props: CountdownProps) => {
-  const [count, setCount] = useState("")
+  const [counter, setCounter] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0
+  })
 
   useEffect(() => {
     setInterval(() => {
@@ -17,17 +22,29 @@ const Countdown = (props: CountdownProps) => {
     const hours = duration.hours();
     const minutes = duration.minutes();
     const seconds = duration.seconds();
-    setCount(days + "日" + hours + "時" + minutes + "分" + seconds + "秒");
+    setCounter({days: days, hours: hours, minutes: minutes, seconds: seconds})
     }, 1000);
   }, [props.targetDate])
 
   
-
   return (
     <div className="countdown">
       <span className="target">{props.target}</span>
       <span> まで あと </span>
-      <span className="count">{count}</span>
+      <span className="count">
+        <span className="count days">
+          {counter.days}日
+        </span>
+        <span className="count hours">
+          {counter.hours}時
+        </span>
+        <span className="count minutes">
+          {counter.minutes}分
+        </span>
+        <span className="count seconds">
+          {counter.seconds}秒
+        </span>
+      </span>
     </div>
   );
 }

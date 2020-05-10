6ff4e4b88a06a76e16dd32356f9c373220973339
ssh-22 <r.march.13_liberty0@yahoo.ne.jp>
2020-05-10 22:41:27 +0900

    make react serve on nginx

--- frontend/covid-19/src/consts.tsx
+++ frontend/covid-19/src/consts.tsx
@@ -1,11 +1,11 @@
 export const CountdownGetAPI = {
-  develop: "http://127.0.0.1:8080/api/countdown/",
+  develop: "http://127.0.0.1:80/api/countdown/",
 };
 
 export const CountdownCreateAPI = {
-  develop: "http://127.0.0.1:8080/api/countdown/create",
+  develop: "http://127.0.0.1:80/api/countdown/create",
 };
 
 export const CountdownDeleteAPI = {
-  develop: "http://127.0.0.1:8080/api/countdown/delete",
+  develop: "http://127.0.0.1:80/api/countdown/delete",
 };
--- src/config/settings.py
+++ src/config/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://localhost",
+    "http://127.0.0.1"
 ]

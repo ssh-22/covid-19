cf1494670c2c76ca00f96f0878f40bd720918a68
ssh-22 <r.march.13_liberty0@yahoo.ne.jp>
2020-05-11 23:49:44 +0900

    rebuild

---
+++ frontend/covid-19/build/precache-manifest.007f395dfc96a069d15c0fac09b4f828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9386ef9ced1387ccb07a37f6e284e7e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "525db37544dc6791d911",
+    "url": "/static/js/2.ee3567ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ee3567ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0c0f00f32047c347893",
+    "url": "/static/js/main.07111ca1.chunk.js"
+  },
+  {
+    "revision": "8f042c284597415f8869",
+    "url": "/static/js/runtime-main.c8325658.js"
+  }
+]);
